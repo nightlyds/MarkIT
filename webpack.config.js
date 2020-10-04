f13bc9891ec80162a7f75b447840da75e1258e2c
Danil <safonov.danil.cw@gmail.com>
2020-10-04 23:16:31 +0300

    Deploy

--- webpack.config.js
+++ webpack.config.js
@@ -5,7 +5,7 @@ const MiniCssExtractPlugin = require("mini-css-extract-plugin");
 module.exports = {
   entry: { main: './src/index.js' },
   output: {
-    path: path.resolve(__dirname, 'build'),
+    path: path.resolve(__dirname, 'dist'),
     filename: 'script.js'
   },
   module: {
